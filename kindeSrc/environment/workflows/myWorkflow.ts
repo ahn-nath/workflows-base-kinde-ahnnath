f2122a561a413f476321bbdb3e7cedc99b492e6e
Nathaly Toledo <nathaly12toledo@gmail.com>
2025-09-03 15:44:56 -0400

    feat: initial code structure
    We have added some test code to explore the outputs of each method and the new structure for the tokens.

--- kindeSrc/environment/workflows/myWorkflow.ts
+++ kindeSrc/environment/workflows/myWorkflow.ts
@@ -7,7 +7,51 @@ export const workflowSettings = {
     }
 };
 
-export default async function Workflow({request, context}) {
-    kinde.accessToken.setCustomClaim("myClaim", "myValue");
-    kinde.idToken.setCustomClaim("myClaim", "myValue");
+// {request, context}
+export default async function Workflow(event) {
+    // Logging for debugging purposes
+    console.log("Token generation workflow with custom code executed");
+    // Init 
+    const kindeAPI = await createKindeAPI(event);
+
+    // [1] Get the relevant details to contruct the user billing claim object
+    // User object
+    const userId = event.context.user.id;
+    const { data: user } = await kindeAPI.get({
+        endpoint: `users/${userId}`,
+    });
+    console.log("User object:", user);
+
+    const customerId = user.customer_id;
+    if (!customerId) {
+        console.log("No customer ID found for user, skipping billing claim construction.");
+        return;
+    }
+    // Entitlements
+    const { data: entitlements } = await kindeAPI.get({
+        endpoint: `billing/entitlements?customer_id=${customerId}`
+    });
+    console.log("Entitlements:", entitlements);
+    // Agreements
+    const { data: agreements } = await kindeAPI.get({
+        endpoint: `billing/agreements?customer_id=${customerId}`
+    });
+    console.log("Agreements:", agreements);
+
+
+    // [2] Construct the user billing claim object 
+    let billingClaimObject = {};
+    // customer ID key with value as a string
+    billingClaimObject["customer_id"] = user.customer_id ? String(user.customer_id) : null;
+    // Billing section from the user object object call
+    billingClaimObject["user_billing"] = user.billing ? user.billing : {};
+    // Entitlements array
+    billingClaimObject['entitlements'] = entitlements ? entitlements : [];
+    // Agreements array
+    billingClaimObject['agreements'] = agreements ? agreements : [];
+    console.log("Billing claim object:", billingClaimObject);
+
+
+    kinde.accessToken.setCustomClaim("billingDetals", billingClaimObject
+    kinde.idToken.setCustomClaim("bILLingDeatils", billingClaimObject);
 }
