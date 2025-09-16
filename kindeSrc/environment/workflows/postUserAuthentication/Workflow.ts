06c0c1e1059d60e32f1934c155ba2de1f73c1a6e
Nathaly Toledo <nathaly12toledo@gmail.com>
2025-09-16 15:20:21 -0400

    minor fix

--- kindeSrc/environment/workflows/postUserAuthentication/Workflow.ts
+++ kindeSrc/environment/workflows/postUserAuthentication/Workflow.ts
@@ -7,6 +7,7 @@ import {
 // The setting for this workflow
 export const workflowSettings: WorkflowSettings = {
   id: "onPostUserAuthentication",
+  
   trigger: WorkflowTrigger.PostAuthentication,
 };
 
--- kindeSrc/environment/workflows/postUserAuthentication/checkIPAgainstAllowlistWorkflow.ts
+++ kindeSrc/environment/workflows/postUserAuthentication/checkIPAgainstAllowlistWorkflow.ts
@@ -7,10 +7,11 @@ import {
 // The setting for this workflow
 export const workflowSettings: WorkflowSettings = {
   id: "onPostUserAuthentication",
+  name: "checkIPAgainstAllowlist",
   trigger: WorkflowTrigger.PostAuthentication,
 };
 
 // The workflow code to be executed when the event is triggered
-export default async function Workflow(event: onPostAuthenticationEvent) {
+export default async function handlePostAuth(event: onPostAuthenticationEvent) {
   console.log("Hello world this is my first line for the workflow `Check IP Against Allowlist Workflow`");
 }
