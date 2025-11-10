08b0d63745d094242abb9f312a5feebf46ea6bba
Nathaly Toledo <nathaly12toledo@gmail.com>
2025-11-10 17:46:21 -0400

    minor fix

--- kindeSrc/environment/workflows/existingPasswordProvided/dripMigrationWorkflow.ts
+++ kindeSrc/environment/workflows/existingPasswordProvided/dripMigrationWorkflow.ts
@@ -41,7 +41,7 @@ export default async function Workflow(event: onExistingPasswordProvidedEvent) {
   console.log("providedPassword", password)
 
 
-  if (hasUserRecordInKinde) {
+  if (!hasUserRecordInKinde) {
     console.log('User exists in Kinde');
     return;
   }
