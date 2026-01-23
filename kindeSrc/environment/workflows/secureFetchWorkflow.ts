88c42f31dba4a5289709b2de19b13f9d9a64a01e
Nathaly Toledo <nathaly12toledo@gmail.com>
2026-01-23 15:44:51 -0400

    feat: add basic call using fetch to reproduce case
    We added a basic API call to reproduce success case for user.

--- kindeSrc/environment/workflows/secureFetchWorkflow.ts
+++ kindeSrc/environment/workflows/secureFetchWorkflow.ts
@@ -10,15 +10,58 @@ export const workflowSettings: WorkflowSettings = {
   name: "Post Authentication - Create/Update User in database",
   trigger: WorkflowTrigger.PostAuthentication,
   bindings: {
-    "kinde.secureFetch": {},
+    // "kinde.secureFetch": {},
     "kinde.env": {},
     url: {},
   },
 };
 
+const TEST_URL = "https://webhook.site/9cc73bd5-80cb-4be4-8e5a-da9c8895f1f0"
+
+
+// 1. The Client's Helper Function (Exact Copy)
+function toURLSearchParams(obj: Record<string, unknown>): URLSearchParams {
+  const params = new URLSearchParams();
+  Object.entries(obj).forEach(([key, value]) => {
+    if (value !== undefined && value !== null) {
+      params.append(key, String(value));
+    }
+  });
+  return params;
+}
+
 export default async function createCustomerUser(event: onPostAuthenticationEvent) {
   console.log(event);
 
-  console.log("This is basic workflow");
+  console.log("Standard Fecth test...");
+
+  const payload = {
+    userID: "randomID",
+    email: "randomEmail",
+    test: "standard_fetch_works"
+  };
+
+  try{
+     const response = await fetch(TEST_URL, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/x-www-form-urlencoded"
+      },
+      body: toURLSearchParams(payload),
+    });
+
+    if(!response.ok){
+      throw new Error(`HTTP Error!: ${response.status}`)
+    }
+
+    const data = await response.json()
+
+    console.log("Success! API response:", JSON.stringify(data, null, 2));
+     
+  }
+  catch(error){
+    console.log("Standard Fetch Failed:", error)
+
+  }
 
 }
